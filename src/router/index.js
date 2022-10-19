388261923524077d6bd609971f75ff74b9cfa674
trip73 <mihail.karpinskii@mail.ru>
2022-10-19 20:32:41 +0300

    Base refactoring

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import RouletteView from '@/views/RouletteView.vue'
+import PlinkoView from '@/views/PlinkoView.vue'
+import CrashView from '@/views/CrashView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Roulette',
+    component: RouletteView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/plinko',
+    name: 'Plinko',
+    component: PlinkoView
+  },
+  {
+    path: '/crash',
+    name: 'Crash',
+    component: CrashView
   }
 ]
 
